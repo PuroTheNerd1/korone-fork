8593c2b7c4371eae4d420cbaa51b3c4581e9ff73
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-02-28 17:11:25 +0100

    save

--- 2016-roblox-main/pages/_app.js
+++ 2016-roblox-main/pages/_app.js
@@ -53,7 +53,7 @@ function RobloxApp({ Component, pageProps }) {
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={''} />
-      <title>{pageProps.title || 'hello world'}</title>
+      <title>{pageProps.title || 'Project X'}</title>
       <link rel='icon' type="image/vnd.microsoft.icon" href='/favicon.ico' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
     </Head>
--- 2016-roblox-main/pages/users/[userId]/profile.js
+++ 2016-roblox-main/pages/users/[userId]/profile.js
@@ -1,17 +1,53 @@
-import { useRouter } from "next/dist/client/router";
-import React from "react";
+import Head from 'next/head';
 import Theme2016 from "../../../components/theme2016";
 import UserProfile from "../../../components/userProfile";
 import UserProfileStore from "../../../components/userProfile/stores/UserProfileStore";
+import { getUserInfo } from "../../../services/users";
 
-const UserProfilePage = props => {
-  const router = useRouter();
-  const userId = router.query['userId'];
-  return <UserProfileStore.Provider>
-    <Theme2016>
-      <UserProfile userId={userId}/>
-    </Theme2016>
-  </UserProfileStore.Provider>
+const UserProfilePage = ({ username, userId }) => {
+  const ogTitle = username || "Project X";
+  const ogUrl = userId ? `https://projex.zip/users/${userId}/profile` : '';
+  const ogDesc = description || "";
+
+  return (
+    <>
+      <Head>
+        <title>{username}</title>
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:url" content={ogUrl} />
+        <meta property="og:type" content="profile" />
+        <meta property="og:description" content="" />
+      </Head>
+      <UserProfileStore.Provider>
+        <Theme2016>
+          <UserProfile userId={userId}/>
+        </Theme2016>
+      </UserProfileStore.Provider>
+    </>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { userId } = context.query;
+
+  try {
+    const info = await getUserInfo({ userId });
+    const username = info.name || "Project X"; 
+    return {
+      props: {
+        username,
+        userId
+      }
+    };
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+    return {
+      props: {
+        username: "Project X", 
+        userId
+      }
+    };
+  }
 }
 
 export default UserProfilePage;
