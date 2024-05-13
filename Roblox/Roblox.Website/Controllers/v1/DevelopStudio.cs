8273cdbde48888ebcada84c36c41dc09a2522682
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-05-13 17:59:35 +0200

    Assets for studio

--- Roblox/Roblox.Website/Controllers/v1/DevelopStudio.cs
+++ Roblox/Roblox.Website/Controllers/v1/DevelopStudio.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Roblox.Dto.Games;
+using Roblox.Models;
+using Roblox.Models.Assets;
 using Roblox.Models.Studio;
 namespace Roblox.Website.Controllers;
 [ApiController]
@@ -32,5 +35,20 @@ public class DevelopStudio : ControllerBase
         var data = new { data = new[] { Templates } };
         string json = JsonConvert.SerializeObject(data);
         return json; 
+    }
+    [HttpGet("user/universes")]
+    public async Task<RobloxCollectionPaginated<GamesForCreatorEntry>> GetUserCreatedGames(long userId,
+        string? sortOrder, string? accessFilter, int limit, string? cursor = null)
+    {
+        if (limit is > 100 or < 1) limit = 10;
+        int offset = int.Parse(cursor ?? "0");
+        var result =
+            (await services.games.GetGamesForType(CreatorType.User, userId, limit, offset, sortOrder ?? "asc", accessFilter ?? "All")).ToList();
+        return new RobloxCollectionPaginated<GamesForCreatorEntry>()
+        {
+            nextPageCursor = result.Count >= limit ? (offset+limit).ToString(): null,
+            previousPageCursor = offset >= limit ? (offset-limit).ToString() : null,
+            data = result,
+        };
     }
 }
