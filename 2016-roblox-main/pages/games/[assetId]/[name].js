7b670e15b04cd53267c03dc81432f4fc6b6b5a6c
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-05-13 22:32:32 +0200

    Added OG:Tags

--- 2016-roblox-main/pages/games/[assetId]/[name].js
+++ 2016-roblox-main/pages/games/[assetId]/[name].js
@@ -1,7 +1,23 @@
+import { useRouter } from 'next/router';
 import SharedAssetPage from "../../../components/sharedAssetPage";
+import Head from 'next/head';
 
-const GamePage = props => {
-  return <SharedAssetPage idParamName='assetId' nameParamName='name' />
+const GamePage = () => {
+  const router = useRouter();
+  const { name, assetId } = router.query;
+
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content={`${name}`} />
+        <meta property="og:url" content={`https://www.projex.zip/games/${assetId}/${name}`} />
+        <meta property="og:type" content="profile" />
+        <meta property="og:image" content={`https://www.projex.zip/Thumbs/Asset.ashx?assetId=${assetId}`} />
+        <meta name="theme-color" content="#f00000" />
+      </Head>
+      <SharedAssetPage idParamName='assetId' nameParamName='name' />
+    </>
+  );
 }
 
 export default GamePage;
