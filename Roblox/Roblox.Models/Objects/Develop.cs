7f44e0506513753a608c813128ab9ffa91227646
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-05-13 14:15:03 +0200

    Studio Apis

---
+++ Roblox/Roblox.Models/Objects/Develop.cs
@@ -0,0 +1,16 @@
+namespace Roblox.Models.Studio;
+public class Universe
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public object description { get; set; }
+    public bool isArchived { get; set; }
+    public int rootPlaceId { get; set; }
+    public bool isActive { get; set; }
+    public string privacyType { get; set; }
+    public string creatorType { get; set; }
+    public int creatorTargetId { get; set; }
+    public string creatorName { get; set; }
+    public DateTime created { get; set; }
+    public DateTime updated { get; set; }
+}
---
+++ Roblox/Roblox.Website/Controllers/v1/DevelopStudio.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Roblox.Models.Studio;
+namespace Roblox.Website.Controllers;
+[ApiController]
+[Route("/v1")]
+public class DevelopStudio : ControllerBase
+{
+    [HttpGetBypass("gametemplates")]
+    public dynamic StudioTemplates()
+    {
+        var Templates = new
+        {
+            gameTemplateType = "Generic",
+            hasTutorials = false,
+            universe = new Universe
+            {
+                id = 28220420,
+                name = "Baseplate",
+                description = null,
+                isArchived = false,
+                rootPlaceId = 95206881,
+                isActive = true,
+                privacyType = "Public",
+                creatorType = "User",
+                creatorTargetId = 998796,
+                creatorName = "Templates",
+                created = DateTime.Parse("2013-11-01T08:47:14.07Z"),
+                updated = DateTime.Parse("2023-05-02T22:03:01.107Z")
+            }
+        };
+        var data = new { data = new[] { Templates } };
+        string json = JsonConvert.SerializeObject(data);
+        return json; 
+    }
+}
