846e45faf398fb2bea44b804add7aab3a83e410b
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-06-06 16:26:45 +0200

    macos

--- Roblox/Roblox.Website/Controllers/Internal/WebController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/WebController.cs
@@ -562,7 +562,7 @@ public class WebController : ControllerBase
         return new
         {
             joinScriptUrl = bootstrapperArgs,
-            prefix = "projectx-client",
+            prefix = "projex-player",
             retroArgs = args
         };
     }
