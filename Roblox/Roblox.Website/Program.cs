5211ea08aeb8b24e41a1570606b4968e05906912
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-06-08 17:58:19 +0200

    logging

--- Roblox/Roblox.Website/Program.cs
+++ Roblox/Roblox.Website/Program.cs
@@ -148,7 +148,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseMiddleware<FrontendProxyMiddleware>();
-//app.UseRobloxLoggingMiddleware();
+app.UseRobloxLoggingMiddleware();
 
 app.UseExceptionHandler("/error");
 //await CommandHandler.Configure("ws://localhost:3189", "hello world of deving 1234");
