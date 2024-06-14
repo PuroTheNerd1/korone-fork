72dead1eecc0766660f0d38bf6c9b771962a285f
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-06-14 19:08:41 +0200

    allow discord bot for embeds

--- Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
@@ -385,7 +385,7 @@ public class ApplicationGuardMiddleware
             }
             
             // If not blocked
-            if (FeatureFlags.IsDisabled(FeatureFlag.AllowAccessToAllRequests) && !ua.ToLower().Contains("roblox"))
+            if (FeatureFlags.IsDisabled(FeatureFlag.AllowAccessToAllRequests) && !ua.ToLower().Contains("roblox") && !ua.ToLower().Contains("Discordbot"))
             {
                 await Redirect(ctx, "/auth/home");
                 return;
