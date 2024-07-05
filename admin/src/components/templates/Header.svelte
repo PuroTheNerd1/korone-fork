0e0aff9f2206816011e1af5ca44424ad457b7b9f
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-05 04:24:32 +0200

    remove color

--- admin/src/components/templates/Header.svelte
+++ admin/src/components/templates/Header.svelte
@@ -22,7 +22,7 @@
 	<title>{title || 'Admin'}</title>
 </svelte:head>
 
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+<header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow">
 	<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/admin">
 		Management
 	</a>
