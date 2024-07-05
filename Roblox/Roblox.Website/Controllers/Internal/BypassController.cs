3b37c9461d3798d247a7f3260e93ae582377ec56
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-06 01:05:45 +0200

    Debug

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -524,6 +524,7 @@ namespace Roblox.Website.Controllers
         public async Task<dynamic> PlaceLaunch(string request, int placeId, string? gameId, bool isPartyLeader, bool isTeleport, string? accessCode, string? linkCode, string? privateGameMode)
         {     
             FeatureFlags.FeatureCheck(FeatureFlag.GamesEnabled, FeatureFlag.GameJoinEnabled);
+            Console.WriteLine(userSession.username);
             if(userSession == null)
             {
                 Redirect("/404");
