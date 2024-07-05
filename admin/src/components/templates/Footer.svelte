02e6c602f62707095c74bfe71e7ed0837441e9f9
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-05 03:51:56 +0200

    fix footer

--- admin/src/components/templates/Footer.svelte
+++ admin/src/components/templates/Footer.svelte
@@ -2,12 +2,12 @@
 	const year = new Date().getFullYear();
 </script>
 
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer mt-auto py-3">
 	<div class="container">
-		<p class="text-muted mb-0">
+		<p class="text-muted mb-0 text-white">
 			<span class="fw-bold">CONFIDENTIAL</span>. All content within this webpage is confidential. Images, recordings, video recordings, photographs, screenshots, or any other forms of copying and/or storing this web page, website, and/or content (physically and/or digitally) are not permitted unless required by law.
 		</p>
-		<p class="text-muted mb-0 mt-2">
+		<p class="text-muted mb-0 mt-2 text-white">
 			&copy;{new Date().getFullYear()}
 			{window.location.hostname}
 		</p>
