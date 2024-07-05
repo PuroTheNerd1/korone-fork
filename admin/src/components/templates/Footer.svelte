000c72df5463bcd63049cf42402ed2b258a36342
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-05 04:22:50 +0200

    wrong color

--- admin/src/components/templates/Footer.svelte
+++ admin/src/components/templates/Footer.svelte
@@ -4,10 +4,10 @@
 
 <footer class="footer mt-auto py-3">
 	<div class="container">
-		<p class="text-muted mb-0 text-white">
+		<p class="mb-0 text-danger">
 			<span class="fw-bold">CONFIDENTIAL</span>. All content within this webpage is confidential. Images, recordings, video recordings, photographs, screenshots, or any other forms of copying and/or storing this web page, website, and/or content (physically and/or digitally) are not permitted unless required by law.
 		</p>
-		<p class="text-muted mb-0 mt-2 text-white">
+		<p class="mb-0 mt-2 text-danger">
 			&copy;{new Date().getFullYear()}
 			{window.location.hostname}
 		</p>
