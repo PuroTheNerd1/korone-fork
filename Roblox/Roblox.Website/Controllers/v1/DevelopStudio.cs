6973d97d5a710f8a71a3de5d4a4789486794f307
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-05 17:55:45 +0200

    Add universe configuration

--- Roblox/Roblox.Website/Controllers/v1/DevelopStudio.cs
+++ Roblox/Roblox.Website/Controllers/v1/DevelopStudio.cs
@@ -93,7 +93,7 @@ public class DevelopStudio : ControllerBase
         { 
             1,
         };
-        return new 
+        return new UniverseConfiguration
         {
             allowPrivateServers = false,
             privateServerPrice = 0,
