ae765f75cc305d832757c6aaa326cd220c54b67d
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-09 03:04:53 +0200

    Yes

---
+++ Roblox/Roblox.Website/Controllers/RobloxApi/StudioScripts.cs
@@ -0,0 +1,64 @@
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Roblox.Dto.Games;
+using Roblox.Dto.Persistence;
+using Roblox.Dto.Users;
+using MVC = Microsoft.AspNetCore.Mvc;
+using Roblox.Libraries.Assets;
+using Roblox.Libraries.FastFlag;
+using Roblox.Libraries.RobloxApi;
+using Roblox.Logging;
+using Roblox.Services.Exceptions;
+using BadRequestException = Roblox.Exceptions.BadRequestException;
+using Roblox.Models.Assets;
+using Roblox.Models.GameServer;
+using Roblox.Models.Users;
+using Roblox.Services;
+using Roblox.Services.App.FeatureFlags;
+using Roblox.Website.Controllers.Internal;
+using Roblox.Website.Filters;
+using Roblox.Website.Middleware;
+using Roblox.Website.WebsiteModels.Asset;
+using Roblox.Website.WebsiteModels.Games;
+using HttpGet = Roblox.Website.Controllers.HttpGetBypassAttribute;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using MultiGetEntry = Roblox.Dto.Assets.MultiGetEntry;
+using SameSiteMode = Microsoft.AspNetCore.Http.SameSiteMode;
+using ServiceProvider = Roblox.Services.ServiceProvider;
+using Type = Roblox.Models.Assets.Type;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+using Roblox.Website.WebsiteModels.Authentication;
+using System.Text.RegularExpressions;
+using InfluxDB.Client.Core.Exceptions;
+using Roblox.Exceptions;
+using Roblox.Website.Pages;
+using System.IO.Compression;
+
+namespace Roblox.Website.Controllers
+{
+
+    [MVC.ApiController]
+    [MVC.Route("/")]
+    public class StudioScripts : ControllerBase
+    {
+        public string visitScript = System.IO.File.ReadAllText(@"C:\ProjectX\services\Roblox\StudioScripts\visit.txt");
+        [HttpGetBypass("game/visit.ashx")]
+        public async Task<dynamic> VisitStudio(int IsPlaySolo, long UserID, long universeId)
+        {
+            var membership2 = await services.users.GetUserMembership(UserID);
+            string finalScript = visitScript.Replace
+                ("%membership%", $"{(int)membership2.membershipType}").Replace
+                ("%userId%", $"{UserID}").Replace
+                ("%universeId%", $"{universeId}");
+            return SignatureController.SignStringResponseForClientFromPrivateKey(finalScript, true);
+        }
+    }
+}
