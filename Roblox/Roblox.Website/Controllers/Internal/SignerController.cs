fa18c0b069143e57ec6f0ab955592f04863b5222
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-21 02:26:53 +0200

    Change ticket

--- Roblox/Roblox.Website/Controllers/Internal/SignerController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/SignerController.cs
@@ -123,10 +123,9 @@ namespace Roblox.Website.Controllers.Internal
         public static string GenerateClientTicketV4(long userId, string username, string characterAppearanceUrl, string jobId, string dateTime, long accountAgeDays, long placeId)
         {
             string membershipType = "Premium";
-            string countryCode = "US";
             string ticket2 = $"{userId}\n{username}\n{characterAppearanceUrl}\n{jobId}\n{dateTime}";
             string ticket2Signature = SignString2048(ticket2);
-            string ticket = $"{dateTime}\n{jobId}\n{userId}\n{userId}\n0\n{accountAgeDays}\nf\n{username.Length}\n{username}\n{membershipType.Length}\n{membershipType}\n{countryCode.Length}\n{countryCode}\n0\n\n{username.Length}\n{username}";
+            string ticket = $"{dateTime}\n{jobId}\n{userId}\n{userId}\n0\n{accountAgeDays}\nf\n{username.Length}\n{username}\n{membershipType.Length}\n{membershipType}\n0\n\n0\n\n{username.Length}\n{username}";
             string ticketSignature = SignString2048(ticket);
 
             string finalTicket = $"{dateTime};{ticket2Signature};{ticketSignature};4";
