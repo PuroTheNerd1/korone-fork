a7afa199307acca213948d2a27476ff98504c9e0
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-25 17:23:05 +0200

    add bot/status

---
+++ Roblox/Roblox.Website/Controllers/Internal/BotApi/WebInfo.cs
@@ -0,0 +1,41 @@
+using MVC = Microsoft.AspNetCore.Mvc;
+
+using Dapper;
+using Npgsql;
+namespace Roblox.Website.Controllers
+{
+
+    [MVC.ApiController]
+    [MVC.Route("/")]
+    public class WebInfo: ControllerBase
+    {
+        private NpgsqlConnection db => services.assets.db;
+        private bool IsBot()
+        {
+            var botKey = Request.Headers.ContainsKey("PJX-BOTAUTH") ? Request.Headers["PJX-BOTAUTH"].ToString() : null;
+            var isBot = botKey == "ljbHjhLvOwPGasmd1qBoa4qkkbcqa1tT39BImr5SvZFbqQXi133GruGL2O2U06906ezZ8pmwEAv33SM5KmWk";
+            return isBot;
+        }
+        [HttpGetBypass("bot/status")]
+        public async Task<dynamic> GetWebInfo()
+        {
+            if(!IsBot())
+                return "Yes";
+            var t = DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(60));
+            var OnlineCount = await db.QuerySingleOrDefaultAsync("SELECT COUNT(*) as total FROM \"user\" WHERE online_at >= :t", new
+            {
+                t,
+            });
+            var Ingame = await db.QuerySingleOrDefaultAsync("SELECT COUNT(*) as total FROM asset_server_player", new
+            {
+                t,
+            });
+            return new 
+            {
+                Online = (long)OnlineCount.Count,
+                Ingame = (long)Ingame.Count
+            };
+        }
+
+    }
+}
