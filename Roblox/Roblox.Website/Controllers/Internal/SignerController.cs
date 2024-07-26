ec9de4deff662f60ebfcf8e25db8c9862bcdd695
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-26 15:13:33 +0200

    Fix ticket

--- Roblox/Roblox.Website/Controllers/Internal/SignerController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/SignerController.cs
@@ -143,15 +143,14 @@ namespace Roblox.Website.Controllers.Internal
             string finalTicket = $"{dateTime};{ticket2Signature};{ticketSignature};3";
             return finalTicket;
         }
-        public static string GenerateClientTicketV4(long userId, string username, string characterAppearanceUrl, string jobId, string dateTime, long accountAgeDays, long placeId)
+        public static string GenerateClientTicketV4(long userId, string username, string characterAppearanceUrl, string membership, string jobId, string dateTime, long accountAgeDays, long placeId)
         {
             DateTime utcNow = DateTime.UtcNow;
                 
             string customTimestamp = utcNow.ToString("MM/dd/yyyy hh:mm:ss tt");
-            string membershipType = "Premium";
             string ticket2 = $"{userId}\n{username}\n{characterAppearanceUrl}\n{jobId}\n{customTimestamp}";
             string ticket2Signature = SignString2048New(ticket2);
-            string ticket = $"{customTimestamp}\n{jobId}\n{userId}\n{userId}\n0\n{accountAgeDays}\nf\n{username.Length}\n{username}\n{membershipType.Length}\n{membershipType}\n0\n\n0\n\n{username.Length}\n{username}";
+            string ticket = $"{customTimestamp}\n{jobId}\n{userId}\n{userId}\n0\n{accountAgeDays}\nf\n{username.Length}\n{username}\n{membership.Length}\n{membership}\n0\n\n0\n\n{username.Length}\n{username}";
             string ticketSignature = SignString2048New(ticket);
 
             string finalTicket = $"{dateTime};{ticket2Signature};{ticketSignature};4";
