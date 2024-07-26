df31972d062a648b83ea1a0d75e0340108c57563
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-26 12:58:10 +0200

    Add files

---
+++ Roblox/Roblox.Website/Documentation/HttpBotAuth.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using System;
+
+namespace Roblox.Website.Controllers
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class BotAuthorizationAttribute : Attribute, IFilterFactory
+    {
+        public bool IsReusable => true;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return new BotAuthorizationFilter();
+        }
+    }
+
+    public class BotAuthorizationFilter : IAuthorizationFilter
+    {
+        private const string BotAuthKey = "ljbHjhLvOwPGasmd1qBoa4qkkbcqa1tT39BImr5SvZFbqQXi133GruGL2O2U06906ezZ8pmwEAv33SM5KmWk";
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.HttpContext.Request.Headers.TryGetValue("PJX-BOTAUTH", out StringValues botKey))
+            {
+                if (botKey != BotAuthKey)
+                {
+                    context.Result = new ForbidResult();
+                }
+            }
+            else
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        [BotAuthorization]  // Applying the custom attribute
+        public IActionResult Get()
+        {
+            return Ok("This is a protected endpoint");
+        }
+    }
+}
