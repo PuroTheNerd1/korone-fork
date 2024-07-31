8d89f69a9aa38700266012c4e4c364c1e9298d8f
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-31 14:54:50 +0200

    Add translate

--- Roblox/Roblox.Website/Middleware/CorsMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/CorsMiddleware.cs
@@ -28,7 +28,7 @@ public class RobloxPlayerCorsMiddleware
         
         // unsafe-eval required by nextjs
         var scriptSrc =
-            "'unsafe-eval' 'self' https://challenges.cloudflare.com/turnstile/v0/api.js https://hcaptcha.com https://*.hcaptcha.com https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js https://projex.zip   http://*.archive.org https://*.archive.org http://js.rbxcdn.com/46eace8231bf3c1ce64c55407d9ae60d.js";
+            "'unsafe-eval' 'self' https://challenges.cloudflare.com/turnstile/v0/api.js https://translate.google.com https://hcaptcha.com https://*.hcaptcha.com https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js https://projex.zip   http://*.archive.org https://*.archive.org http://js.rbxcdn.com/46eace8231bf3c1ce64c55407d9ae60d.js";
         
         return "default-src 'self'; img-src "+imgSrc+"; child-src 'self'; script-src "+scriptSrc+"; frame-src 'self' https://hcaptcha.com https://challenges.cloudflare.com http://challenges.cloudflare.com  https://challenges.cloudflare.com/* http://web.archive.org https://*.archive.org https://web.archive.org/* https://*.hcaptcha.com; style-src 'unsafe-inline' 'self'  http://*.archive.org https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css ; font-src 'self' fonts.gstatic.com; connect-src "+connectSrc+"; worker-src 'self';";
     }
