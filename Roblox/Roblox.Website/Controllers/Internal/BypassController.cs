826594e27fabe73c3c1ce30a0e8ca6f8b166308e
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-07-31 20:28:18 +0200

    change flags

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -1091,10 +1091,11 @@ namespace Roblox.Website.Controllers
             {
                 case 2015:
                 case 2016:
+                    return SignatureController.SignJsonResponseForClientFromPrivateKey(joinScript2016);
                 case 2017:
-                    return SignatureController.SignJsonResponseForClientFromPrivateKey(joinScript20192020);
+                    return SignatureController.SignJsonResponseForClientFromPrivateKey(joinScript20172018);
                 case 2018:
-                    return SignatureController.SignJson2048(joinScript20192020);      
+                    return SignatureController.SignJson2048(joinScript20172018);      
                 case 2020:
                     return SignatureController.SignJson2048New(joinScript20192020);
                 default:
