4f924bcf8e648d43596bd188203c81d4eb6c0ddd
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 04:28:40 +0200

    Log ua

--- Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
@@ -354,7 +354,7 @@ public class ApplicationGuardMiddleware
                 if (deleteCookie)
                     ctx.Response.Cookies.Delete("uabypass1");
             }
-            Console.WriteLine(ua.ToString());
+            Console.WriteLine($"user agent:{ua.ToString()}");
             uaBypassWatch.Stop();
             Writer.Info(LogGroup.AbuseDetection, "took {0}ms to parse ua bypass cookie", uaBypassWatch.ElapsedMilliseconds);
         }
