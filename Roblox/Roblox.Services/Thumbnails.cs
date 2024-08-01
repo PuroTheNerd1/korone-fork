c4effc35c436dcb91e5118cbf6b3e5c0ebecaf0e
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 10:35:00 +0200

    Add baseurl

--- Roblox/Roblox.Services/Thumbnails.cs
+++ Roblox/Roblox.Services/Thumbnails.cs
@@ -79,7 +79,7 @@ public class ThumbnailsService : ServiceBase, IService
         {
             c.state = c.imageUrl == null ? ThumbnailState.Pending : ThumbnailState.Completed;
             if (c.imageUrl != null)
-                c.imageUrl = Roblox.Configuration.CdnBaseUrl + c.imageUrl;
+                c.imageUrl = Roblox.Configuration.BaseUrl + c.imageUrl;
             return c;
         });
     }
