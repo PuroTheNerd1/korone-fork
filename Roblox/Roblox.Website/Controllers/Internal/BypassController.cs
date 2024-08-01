a847ae810b486c3d77986244b2222dc7496e5e85
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 11:19:41 +0200

    Fix assets

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -462,7 +462,7 @@ namespace Roblox.Website.Controllers
                     Console.WriteLine(request.assetId);
                     assetReturnInfo.Add(new
                     {
-                        Location = $"{Configuration.BaseUrl}/sajdajkdjadj",
+                        Location = $"{Configuration.BaseUrl}/v1/asset?assetId={request.assetId}",
                         RequestId = request.requestId
                     });
                 }
@@ -470,7 +470,7 @@ namespace Roblox.Website.Controllers
                 {
                     assetReturnInfo.Add(new
                     {
-                        Location = $"{Configuration.BaseUrl}/sajdajkdjadj",
+                        Location = $"{Configuration.BaseUrl}/v1/asset?assetId={request.assetId}",
                         RequestId = request.requestId
                     });
                     continue;
