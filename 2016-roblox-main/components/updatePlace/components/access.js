9f10d0e87bf08633aac5541c95ce9008b3f007ee
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 14:58:17 +0200

    Prepare site

--- 2016-roblox-main/components/updatePlace/components/access.js
+++ 2016-roblox-main/components/updatePlace/components/access.js
@@ -76,6 +76,7 @@ const Access = (props) => {
             <option value={2017}>2017</option>
             <option value={2018}>2018</option>
             <option value={2020}>2020</option>
+            <option value={2021}>2021</option>
           </select>
         </div>
         <div className="mt-4">
