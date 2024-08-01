76eed2d9c2b3744911be54745e386c16336cec9f
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 09:45:45 +0200

    Alert for mobile

---
+++ Roblox/Roblox.Website/Controllers/RobloxApi/SiteInfo.cs
@@ -0,0 +1,25 @@
+using MVC = Microsoft.AspNetCore.Mvc;
+using Roblox.Website.Controllers.Internal;
+using CsvHelper;
+using System.Xml;
+namespace Roblox.Website.Controllers
+{
+
+    [MVC.ApiController]
+    [MVC.Route("/")]
+    public class SiteAlertMobile: ControllerBase
+    {
+        [HttpGetBypass("alerts/alert-info")]
+        public async Task<dynamic> GetAlert()
+        {
+            var alert = await services.users.GetGlobalAlert();
+            return new
+            {
+                IsVisible = alert != null,
+                Text = alert?.message ?? "",
+                LinkText = "",
+                LinkUrl = alert?.url ?? "",
+            };
+        }
+    }
+}
