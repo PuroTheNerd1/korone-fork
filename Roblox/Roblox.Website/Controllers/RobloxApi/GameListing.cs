e40d5c0ce7c93c8952c730429b14832b4830e6d3
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 11:47:20 +0200

    Add more universe apis

--- Roblox/Roblox.Website/Controllers/RobloxApi/GameListing.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/GameListing.cs
@@ -4,22 +4,13 @@ using CsvHelper;
 using System.Xml;
 using Roblox.Services.Exceptions;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using Roblox.Dto.Users;
-using Roblox.Exceptions;
-using Roblox.Services;
-using Roblox.Services.App.FeatureFlags;
-using Roblox.Services.Exceptions;
-using BadRequestException = Roblox.Exceptions.BadRequestException;
-using ServiceProvider = Roblox.Services.ServiceProvider;
-
 using Roblox.Dto.Marketplace;
 using Newtonsoft.Json;
 using System.Dynamic;
 using Roblox.Models;
 using Roblox.Dto.Friends;
 using Roblox.Models.Assets;
+using System.Text.RegularExpressions;
 namespace Roblox.Website.Controllers
 {
 
@@ -27,6 +18,28 @@ namespace Roblox.Website.Controllers
     [MVC.Route("/")]
     public class GameListing: ControllerBase
     {
+        private static Regex numberRegex { get; } = new("([0-9]+)");
+        [HttpGet("v1/games/multiget-playability-status")]
+        public dynamic MultiGetPlayabilityStatus()
+        {
+            var ids = HttpContext.Request.QueryString.Value;
+            return numberRegex.Matches(ids).Select(c => long.Parse(c.Value)).Distinct().Select(c => new
+            {
+                playabilityStatus = "Playable",
+                isPlayable = true,
+                universeId = c,
+            });
+        }
+        [HttpGet("v1/games")]
+        public async Task<dynamic> MultiGetUniverseInfo(string universeIds)
+        {
+            var sp = universeIds.Split(",").Select(long.Parse);
+            var result = await services.games.MultiGetUniverseInfo(sp);
+            return new
+            {
+                data = result,
+            };
+        }
         [HttpGetBypass("v1/games/sorts")]
         public dynamic GameSort()
         {
@@ -34,6 +47,7 @@ namespace Roblox.Website.Controllers
             dynamic? json = JsonConvert.DeserializeObject<ExpandoObject>(rawJson);
             return json ?? new ExpandoObject();
         }
+
         [HttpGetBypass("v1/games/list")]
         public async Task<dynamic> GetGamesList(string? sortToken, int maxRows = 10, Genre? genre = null, string? keyword = null)
         {
