b4efaa81d2c0b876dcf222c3b8168669caf702d9
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-01 02:34:57 +0200

    Add well known

---
+++ Roblox/Roblox.Website/Controllers/WellKnown/Wellknown.cs
@@ -0,0 +1,39 @@
+using MVC = Microsoft.AspNetCore.Mvc;
+using Roblox.Website.Controllers.Internal;
+using CsvHelper;
+using System.Xml;
+using Roblox.Services.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Roblox.Dto.Users;
+using Roblox.Exceptions;
+using Roblox.Services;
+using Roblox.Services.App.FeatureFlags;
+using Roblox.Services.Exceptions;
+using BadRequestException = Roblox.Exceptions.BadRequestException;
+using ServiceProvider = Roblox.Services.ServiceProvider;
+
+using Roblox.Dto.Marketplace;
+using Newtonsoft.Json;
+using System.Dynamic;
+namespace Roblox.Website.Controllers
+{
+
+    [MVC.ApiController]
+    [MVC.Route("/")]
+    public class WellKnown: ControllerBase
+    {
+        [HttpGetBypass(".well-known/discord")]
+        public string WellKnownDiscord()
+        {
+            string hostName = HttpContext.Request.Host.Host;
+            
+            if (hostName.Contains("phil564"))
+            {
+                return "dh=20873664bf98d2f31ebdd1c2df0ddd62821db03b";
+            }
+            return "dh=20873664bf98d2f31ebdd1c2df0ddd62821db03b";
+        }
+    }
+}
