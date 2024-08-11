892befe500c1adc6174fe45a5b64c7a393a3e177
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-11 07:54:34 +0200

    Put base url

--- Roblox/Roblox.Website/Controllers/RobloxApi/Thumbnails.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Thumbnails.cs
@@ -146,6 +146,10 @@ public class RbxThumbnails : ControllerBase
         var parsed = universeIds.Split(",").Select(long.Parse).Distinct().ToList();
         if (parsed.Count is > 200 or < 0) throw new BadRequestException();
         var result = await services.thumbnails.GetGameIcons(parsed);
+        foreach(var item in result)
+        {
+            item.imageUrl = Configuration.BaseUrl + item.imageUrl;
+        }
         return new()
         {
             data = result,
