a477fe6d219f4d9f3b467b7d1f61ca9f1e188e3e
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-11 11:23:51 +0200

    ticket test

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -926,8 +926,9 @@ namespace Roblox.Website.Controllers
                     finalTicket = services.sign.GenerateClientTicketV4(userId, username, characterAppearanceUrl, membership, jobId, formattedDateTime, accountAgeDays, placeId);
                     break;
                 case 2021:
-                    characterAppearanceUrl = $"http://www.projex.zip/v1/avatar-fetch?userId={placeId}&placeId={placeId}";
-                    finalTicket = services.sign.GenerateClientTicketV4(userId, username, characterAppearanceUrl, membership, jobId, formattedDateTime, accountAgeDays, placeId);
+                    //characterAppearanceUrl = $"http://www.projex.zip/v1/avatar-fetch?userId={placeId}&placeId={placeId}";
+                    //finalTicket = services.sign.GenerateClientTicketV4(userId, username, characterAppearanceUrl, membership, jobId, formattedDateTime, accountAgeDays, placeId);
+                    finalTicket = "daslkdjaskldjsakd";
                     break;
                 default:
                     throw new InvalidOperationException($"This year does not exist: {year}");
