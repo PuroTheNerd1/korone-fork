9f4189091e597a87796362954c928d1bbabc3c17
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-11 05:59:51 +0200

    It works now revert

--- Roblox/Roblox.Services/Games/Games.cs
+++ Roblox/Roblox.Services/Games/Games.cs
@@ -418,70 +418,8 @@ public class GamesService : ServiceBase, IService
             GameLocale = "en_us",
             CountryCode = "US"
         };
-        dynamic joinScript20192020 = new
-        {
-            ClientPort = 0,
-            MachineAddress = Configuration.GameServerIp,
-            ServerConnections = new List<dynamic>
-            {
-                new
-                {
-                    Port = GameServerService.currentGameServerPorts[jobId], 
-                    Address = Configuration.GameServerIp, 
-                }
-            },
-            ServerPort = GameServerService.currentGameServerPorts[jobId], 
-            PingUrl = "", 
-            PingInterval = 120, 
-            UserName = username, 
-            DisplayName = username,
-            SeleniumTestMode = false, 
-            UserId = userId, 
-            ClientTicket = finalTicket, 
-            SuperSafeChat = false, 
-            PlaceId = placeId, 
-            MeasurementUrl = "",
-            WaitingForCharacterGuid = Guid.NewGuid().ToString(),
-            BaseUrl = Configuration.BaseUrl, 
-            ChatStyle = "ClassicAndBubble", 
-            VendorId = 0,
-            ScreenShotInfo = "",
-            VideoInfo = "",
-            CreatorId = builderId,
-            CreatorTypeEnum = "User",  
-            MembershipType = membership, 
-            AccountAge = accountAgeDays, 
-            CookieStoreFirstTimePlayKey = "rbx_evt_ftp",
-            CookieStoreFiveMinutePlayKey = "rbx_evt_fmp",
-            CookieStoreEnabled = true,
-            IsRobloxPlace = false,
-            UniverseId = universeId,
-            GenerateTeleportJoin = false,
-            IsUnknownOrUnder13 = false,
-            SessionId = $"{Guid.NewGuid().ToString()}|{jobId}|0|45.137.70.23|8|{formattedDateTime}|0|null|{cookie}|null|null|null",
-            DataCenterId = 0,
-            FollowUserId = 0,
-            BrowserTrackerId = 0,
-            UsePortraitMode = false,
-            CharacterAppearance = $"http://www.projex.zip/v1/avatar-fetch?userId={placeId}&placeId={placeId}",
-            GameId = jobId,     
-            RobloxLocale = "RobloxLocale",
-            GameLocale = "en_us",
-            CountryCode = "US",
-            characterAppearanceId = userId,
-        };
-        switch (year)
-        {
-            case 2016:
-            case 2017:
-            case 2018:
-            case 2020:
-                return joinScript;
-            case 2021:
-                return joinScript;
-            default:
-                return joinScript;
-        }
+
+        return joinScript;
     }
 
     public dynamic SignJoinScript(long year, dynamic joinScript)
