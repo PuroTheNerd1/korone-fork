7a4d233e240d61e31168b534e4b822876ab5618d
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-11 15:13:05 +0200

    Fix prem on older games

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -904,7 +904,8 @@ namespace Roblox.Website.Controllers
             {
                 membership = (int)membership2!.membershipType == 4 ? "Premium" : (int)membership2!.membershipType == 3 ? "OutrageousBuildersClub" : (int)membership2.membershipType == 2 ? "TurboBuildersClub" : (int)membership2.membershipType == 1 ? "BuildersClub" : "None";
             }
-            if(year != 2020 || year != 2021 && membership == "Premium")
+            Console.WriteLine(membership);
+            if(year != 2020 && year != 2021 && membership == "Premium")
             {
                 membership = "OutrageousBuildersClub";
             }
