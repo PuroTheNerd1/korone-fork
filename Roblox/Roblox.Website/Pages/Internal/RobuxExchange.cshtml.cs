210b3681b225c410824e0c2a63d4b449f8c193d6
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-16 21:42:30 +0200

    Add robux exchange

---
+++ Roblox/Roblox.Website/Pages/Internal/RobuxExchange.cshtml.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Roblox.Dto.Users;
+using Roblox.Models.Users;
+using System.Threading.Tasks;
+
+namespace Roblox.Website.Pages.Internal
+{
+    public class RobuxExchange : RobloxPageModel
+    {
+        [BindProperty]
+        public string? successMessage { get; set; }
+        [BindProperty]
+        public string? errorMessage { get; set; }
+        [BindProperty]
+        public long robux { get; set; }
+
+        public void OnGet()
+        {
+            if (userSession == null)
+            {
+                return;
+            }
+        }
+
+        public async Task OnPost()
+        {
+            if (userSession == null)
+            {
+                return;
+            }
+
+            if (robux < 10)
+            {
+                errorMessage = "The minimum tix you can exchange is 10.";
+                return;
+            }
+
+            int conversionRate = 10;
+            decimal roughTix = robux * conversionRate;
+            long finaltix = (long)Math.Round(roughTix, 0);
+
+            try
+            {
+                var balance = await services.economy.GetUserBalance(userSession.userId);
+                long newBalance = balance.tickets;
+
+                if (newBalance < robux)
+                {
+                    errorMessage = "Insufficient tix balance.";
+                    return;
+                }
+
+                await services.economy.DecrementCurrency(Models.Assets.CreatorType.User, userSession.userId, Models.Economy.CurrencyType.Robux, robux);
+                await services.economy.IncrementCurrency(Models.Assets.CreatorType.User, userSession.userId, Models.Economy.CurrencyType.Tickets, finaltix);
+
+                successMessage = $"You have received {finaltix} tickets from {robux} R$.";
+                return;
+            }
+            catch (Exception)
+            {
+                errorMessage = "Failed to convert tix to robux.";
+                return;
+            }
+        }
+    }
+}
