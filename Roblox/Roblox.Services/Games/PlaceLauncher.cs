16f67eb9100fa60f185769f3deabf02ff0de9d3a
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-17 02:03:12 +0200

    Add game end check

--- Roblox/Roblox.Services/Games/PlaceLauncher.cs
+++ Roblox/Roblox.Services/Games/PlaceLauncher.cs
@@ -46,6 +46,14 @@ public class PlaceLauncherService : ServiceBase
     public async Task<PlaceLaunchResponse> RequestGameJob(string gameId, long placeId)
     {
         GamesService games = new GamesService();
+        if (!GameServerService.currentGameServerPorts.ContainsKey(gameId)){
+            //if the job id is missing the game most likely ended
+            return new PlaceLaunchResponse()
+            {
+                status = (int)JoinStatus.GameEnded,
+                message = "The game is ended",
+            };
+        }
         if (await games.IsFull(gameId, placeId))
         {
             return new PlaceLaunchResponse()
