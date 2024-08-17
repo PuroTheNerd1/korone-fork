1ca0b3c005589c9dae6e7939deb7f846ea98fdaa
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-08-17 23:51:52 +0200

    Add the year check

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -631,6 +631,15 @@ namespace Roblox.Website.Controllers
         [HttpPostBypass("/v1/join-game")]
         public async Task<PlaceLaunchResponse> JoinGameMobile([FromBody] JoinGame request)
         {
+            long year = await services.games.GetYear(request.placeId);
+            if (year != 2020 && year != 2021)
+            {
+                return new PlaceLaunchResponse()
+                {
+                    status = (int)JoinStatus.Error,
+                    message = "An error occured while starting the game."
+                };
+            }
             var placeLauncherRequest = new PlaceLaunchRequest
             {
                 request = "RequestGame",
