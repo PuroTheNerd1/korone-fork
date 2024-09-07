961139d0c1eee3eeb34d8e69ab1114a9e66cb177
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-09-07 21:01:59 +0200

    Fix

--- Roblox/Roblox.Website/Controllers/Internal/AdminApi.cs
+++ Roblox/Roblox.Website/Controllers/Internal/AdminApi.cs
@@ -352,6 +352,7 @@ public class AdminApiController : ControllerBase
                     serial = serial
                 }
             );
+            await services.assets.IncrementSaleCount(req.assetId);
         }
         return Ok();
     }
