55116808d5e8032150028323f9d3920f19d932d6
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-02 18:05:03 +0200

    add files

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -1688,7 +1688,7 @@ namespace Roblox.Website.Controllers
                 "a9912debcb6347c402e4139f452d4fd2", //2015M Prod
                 "d902c5a3a4a33954bc6fbd0daa485966", //2016E Prod
                 //"8462b22b8d503729debafd46a8c245c8", //2017L RAGELoader Debug
-                "2fb881c2aeb6daf52cdc8f2ee6e3c3ec", //2017L Prod
+                "2aa77a66b7a07f996b87f63a3e1a867e", //2017L Prod
                 "8c5aecb7811acbb582f06f2a81b958f4", //2018L Prod
                 "4022369076d608d1a99b7b3d250e4de5", //2018L RAGELoader Debug
                 "9cdc73fd9b24c974f5a0dde411dcd38f", //2020L Prod
