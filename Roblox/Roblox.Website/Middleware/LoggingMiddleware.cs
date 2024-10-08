0892adae5ef4e4e6f8127f463eed74bc4ec02cf7
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-08 23:51:54 +0200

    log all voice apis

--- Roblox/Roblox.Website/Middleware/LoggingMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/LoggingMiddleware.cs
@@ -25,6 +25,10 @@ public class RobloxLoggingMiddleware
         {
             return;
         }
+        if(!ctx.Request.GetEncodedUrl().Contains("voice"))
+        {
+            return;
+        }
         Console.WriteLine(str);
     }
 }
