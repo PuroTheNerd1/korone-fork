ffe24f38afb55d7686bf95b92b3ba193ae5b48f9
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-16 20:02:24 +0200

    Make gamesearch better

--- Roblox/Roblox.Services/Games/Games.cs
+++ Roblox/Roblox.Services/Games/Games.cs
@@ -182,7 +182,7 @@ public class GamesService : ServiceBase, IService
         {
             query.Where("asset.name ILIKE :keyword", new
             {
-                keyword = keyword + "%",
+                keyword = "%" + keyword + "%",
             });
         }
         
