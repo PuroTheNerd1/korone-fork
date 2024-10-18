1375ca74be886462170decf347770dcaa373f2d0
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-18 14:31:00 +0200

    implemented gambling

--- Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
@@ -1,3 +1,4 @@
+using InfluxDB.Client.Core.Exceptions;
 using MVC = Microsoft.AspNetCore.Mvc;
 
 namespace Roblox.Website.Controllers
@@ -9,17 +10,54 @@ namespace Roblox.Website.Controllers
     {
         [BotAuthorization]
         [HttpGetBypass("bot/coinflip")]
-        public async Task<dynamic> CoinFlip(string discordid)
+        public async Task<dynamic> CoinFlip(string discordid, int amount)
         {
             Random random = new Random();
-            double chance = random.NextDouble();
-            if (chance < 0.25)
+            Dto.Users.UserInfo userInfo;
+
+            if (amount > 1000 || amount < 1)
             {
-                return "Heads";
+                return new { error = "Invalid amount" };
+            }
+
+            try 
+            {
+                userInfo = await services.users.GetUserByDiscordId(discordid);
+            }
+            catch (Exception e)
+            {
+                throw new BadRequestException("User not found");
+            }
+
+            var balance = await services.economy.GetUserBalance(userInfo.userId);
+            long newBalance = balance.robux;
+            //balance check
+            if (newBalance < amount)
+            {
+                throw new BadRequestException("Insufficient balance");
+            }
+            //decrement currency here
+            await services.economy.DecrementCurrency(Models.Assets.CreatorType.User, userInfo.userId, Models.Economy.CurrencyType.Robux, amount);
+            //calculate if win
+            int chance = random.Next(0, 101);
+            bool isWinner = chance <= 40;
+            int finalRobux = amount * 2;
+            if (isWinner)
+            {
+                await services.economy.IncrementCurrency(Models.Assets.CreatorType.User, userInfo.userId, Models.Economy.CurrencyType.Robux, finalRobux);
+                return new
+                {
+                    message = "You won!",
+                    newBalance = newBalance + finalRobux
+                };
             }
             else
             {
-                return "Tails";
+                return new
+                {
+                    message = "You lost!",
+                    newBalance = newBalance + finalRobux
+                };
             }
         }
     }
