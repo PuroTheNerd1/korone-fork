e1ab6ae459a890a4ed9f66526ac81b9e5243da72
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-21 17:14:23 +0200

    edit script

--- RCCService/RCCService2020/start.bat
+++ RCCService/RCCService2020/start.bat
@@ -1,4 +1,4 @@
 echo "Starting Player Thumbnail RCC"
-start /b RCCService.exe -console -port 10000
+start /b RCCService.exe -console -verbose -port 10000
 echo "Starting Player Headshot RCC"
-start /b RCCService.exe -console -port 10001
+start /b RCCService.exe -console -verbose -port 10001
