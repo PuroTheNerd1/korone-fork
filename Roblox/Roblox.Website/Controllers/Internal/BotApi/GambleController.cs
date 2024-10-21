086de16ea4adecb2b6fb6afefda1f439629e33ab
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-21 14:29:01 +0200

    Whoopsie finalRobux -> amount

--- Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
@@ -71,7 +71,7 @@ namespace Roblox.Website.Controllers
                 return new GamblingResponse
                 {
                     message = "You have flipped tails and lost",
-                    submessage = $"\nYou have lost **{amount}** R$, your balance is updated to **{(newBalance - finalRobux).ToString("N0", CultureInfo.CurrentCulture)}** R$",
+                    submessage = $"\nYou have lost **{amount}** R$, your balance is updated to **{(newBalance - amount).ToString("N0", CultureInfo.CurrentCulture)}** R$",
                     status = (int)GamblingStatus.Lost, 
                 };
             }
