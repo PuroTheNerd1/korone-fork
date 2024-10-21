3b2c4057e5f25054bca815982a9d741a796caba8
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-21 14:31:16 +0200

    Whoopsie finalRobux -> amount

--- Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BotApi/GambleController.cs
@@ -62,7 +62,7 @@ namespace Roblox.Website.Controllers
                 return new GamblingResponse
                 {
                     message = "You have flipped heads and won!",
-                    submessage = $"\nYou have won **{finalRobux}** R$, your balance is updated to **{(newBalance + amount).ToString("N0", CultureInfo.CurrentCulture)}** R$",
+                    submessage = $"\nYou have won **{finalRobux}** R$, your balance is updated to **{(newBalance + finalRobux).ToString("N0", CultureInfo.CurrentCulture)}** R$",
                     status = (int)GamblingStatus.Won, // Explicitly cast to int
                 };
             }
