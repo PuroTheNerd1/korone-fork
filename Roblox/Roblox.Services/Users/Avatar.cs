176e15b134483a92dbb41d7c3d7eb8c73b4db6e0
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-30 15:54:07 +0100

    Put to strech

--- Roblox/Roblox.Services/Users/Avatar.cs
+++ Roblox/Roblox.Services/Users/Avatar.cs
@@ -686,7 +686,7 @@ public class AvatarService : ServiceBase, IService
                 image.Mutate(x => x.Resize(new ResizeOptions
                 {
                     Size = new Size(newX, newY),
-                    Mode = ResizeMode.Crop
+                    Mode = ResizeMode.Stretch
                 }));
 
                 using (MemoryStream resizedMemoryStream = new MemoryStream())
