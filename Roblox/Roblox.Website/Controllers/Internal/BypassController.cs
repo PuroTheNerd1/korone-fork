67692b970376dbce18eddb8ea1c69bd01d6a6395
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-10-30 20:56:22 +0100

    add rageloader

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -1668,7 +1668,7 @@ namespace Roblox.Website.Controllers
             {
                 "a9912debcb6347c402e4139f452d4fd2", //2015M Prod
                 "d902c5a3a4a33954bc6fbd0daa485966", //2016E Prod
-                "6291a508a450e8bccccaf4788255a641", //2017L RAGELoader Debug
+                "fc5f43ec839bbbffcb26c48846b3c865", //2017L RAGELoader Debug
                 "81b26cbf32171b1691d4b5d135e0ef84", //2017L Prod
                 "8c5aecb7811acbb582f06f2a81b958f4", //2018L Prod
                 "4022369076d608d1a99b7b3d250e4de5", //2018L RAGELoader Debug
