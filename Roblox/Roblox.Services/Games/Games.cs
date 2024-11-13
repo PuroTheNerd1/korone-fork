7b71031850b94b3c6e62e538a50d73b57e7aa535
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-11-13 12:45:36 +0100

    Ughhh

--- Roblox/Roblox.Services/Games/Games.cs
+++ Roblox/Roblox.Services/Games/Games.cs
@@ -458,7 +458,6 @@ public class GamesService : ServiceBase, IService
             name = c.name,
             description = c.description,
             rootPlaceId = c.rootAssetId,
-            isActive = true,
             privacyType = c.isPublic ? PrivacyType.Public : PrivacyType.Private,
             creatorType = (int) creatorType,
             creatorTargetId = creatorId,
