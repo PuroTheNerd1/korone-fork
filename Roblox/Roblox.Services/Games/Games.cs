016ae81a8c7ce57a240e9acccc9e4a3fd99689f7
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-11-13 10:53:16 +0100

    Format sql statement

--- Roblox/Roblox.Services/Games/Games.cs
+++ Roblox/Roblox.Services/Games/Games.cs
@@ -407,7 +407,13 @@ public class GamesService : ServiceBase, IService
         int offset, string? sort, string? accessFilter)
     {
         var qu = await db.QueryAsync<GamesForCreatorEntryDb>(
-            "SELECT u.id, a.name, a.description, u.root_asset_id as rootAssetId, ap.visit_count as visitCount, a.created_at as created, a.updated_at as updated FROM universe AS u INNER JOIN asset a ON a.id = u.root_asset_id INNER JOIN asset_place ap ON ap.asset_id = u.root_asset_id WHERE u.creator_type = :type AND u.creator_id = :id LIMIT :limit OFFSET :offset", new
+            @"SELECT u.id, a.name, a.description, u.root_asset_id as rootAssetId, ap.visit_count as visitCount, a.created_at as created, a.updated_at as updated
+            FROM universe AS u
+            INNER JOIN asset a ON a.id = u.root_asset_id
+            INNER JOIN asset_place ap ON ap.asset_id = u.root_asset_id
+            WHERE u.creator_type = :type AND u.creator_id = :id
+            LIMIT :limit OFFSET :offset",
+            new
             {
                 type = creatorType,
                 id = creatorId,
