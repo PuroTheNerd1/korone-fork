bb25f9f32114854e4725f0269bcb819f47b075c3
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-11-19 19:34:55 +0100

    aa

--- 2016-roblox-main/components/myDashboard/index.js
+++ 2016-roblox-main/components/myDashboard/index.js
@@ -197,7 +197,7 @@ const useStyles = createUseStyles({
 })
 
 const sorts = [{
-    token: 'RecentlyPlayed',
+    token: 'recent',
     name: 'recently_played',
     displayName: 'Recently Played',
     games: [],
