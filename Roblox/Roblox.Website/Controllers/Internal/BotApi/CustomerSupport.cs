8e2e09ce9c3ed6c082a97d4acbc43097c0bf2c0c
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-11-25 12:10:35 +0100

    Finish customer support

--- Roblox/Roblox.Website/Controllers/Internal/BotApi/CustomerSupport.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BotApi/CustomerSupport.cs
@@ -15,19 +15,36 @@ namespace Roblox.Website.Controllers
         [HttpGet("bot/resetpassword")]
         public async Task<dynamic> ResetPassword(string discordId, long userId)
         {
-            UserInfo userDiscordInfo = await services.users.GetUserByDiscordId(discordId);
+            UserInfo userDiscordInfo = null;
+            // Why are we handling this?
+            // Because GetUserById/GetUserByDiscordId will throw the RecordNotFound exception and then the message will be "NotFound" and that will be in the embed which we don't want
+            try
+            {
+                userDiscordInfo = await services.users.GetUserByDiscordId(discordId);
+            }
+            // TODO: Make DiscordNotLinkedException
+            catch (RecordNotFoundException)
+            {
+                return new
+                {
+                    success = false,
+                    message = "Your account is not linked, please use the /linkaccount command to link your account",
+                };
+            }
+
             // First we check if the user who ran the command if he is a owner if they are not, then throw exception
             if (!StaffFilter.IsOwner(userDiscordInfo.userId))
                 throw new RobloxException(403, 1, "You are not allowed to reset passwords");
             // This is a extra security check, let's check if the user who we are trying to reset is a staff member if they are then throw a exception
             if (await StaffFilter.IsStaff(userId))
                 throw new RobloxException(403, 1, "You are not allowed to reset the password of this user");
-            string randomlyGeneratedPassword = Guid.NewGuid().ToString().Replace("-", "") + Guid.NewGuid().ToString().Replace("-", "");
+            // Example: 19bcbfac216d46cbaeb826125d1bae42
+            string randomlyGeneratedPassword = (Guid.NewGuid().ToString().Replace("-", "") + Guid.NewGuid().ToString().Replace("-", "")).Substring(0, 32);
             UserInfo userInfo = await services.users.GetUserById(userId);
             await services.users.UpdatePassword(userId, randomlyGeneratedPassword);
             return new
             {
-                status = 0,
+                success = true,
                 password = randomlyGeneratedPassword,
                 message = $"The password has been successfully reset of **{userInfo.username}**.\nThe password has been sent in your DM's"
             };
