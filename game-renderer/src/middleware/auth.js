2b0ad900d0833bbb868258f80c4908917180418a
johnhamilcar <johnhamilcar@gmail.com>
2024-12-01 03:43:05 -0500

    Removed authorization from game-renderer

--- game-renderer/src/middleware/auth.js
+++ game-renderer/src/middleware/auth.js
@@ -1,8 +1,9 @@
-import response from '../util/response.js'
-import conf from '../util/config.js'
+//import response from '../util/response.js'
+//import conf from '../util/config.js'
 
 const auth = async (req, res, next) => {
-    const { authorization, userAgent } = req.headers
+    next()
+    /*const { authorization, userAgent } = req.headers
     if (authorization && authorization.startsWith('Bearer ')) {
         let token = authorization.slice(7)
         if (token && token === conf.authorization) {
@@ -12,7 +13,7 @@ const auth = async (req, res, next) => {
         }
     } else {
         return response(res, 'You are not authorized to use game-renderer!', 401, false)
-    }
+    }*/
 }
 
 export default auth;
