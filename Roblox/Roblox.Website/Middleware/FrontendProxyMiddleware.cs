fa3071a0259ea72c98116aa77cb0952ef171484a
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-12-02 19:39:37 +0100

    forgot /

--- Roblox/Roblox.Website/Middleware/FrontendProxyMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/FrontendProxyMiddleware.cs
@@ -188,7 +188,7 @@ public class FrontendProxyMiddleware
     {
         var requestUrl = ctx.Request.GetEncodedPathAndQuery();
         var requestFullUrl = ctx.Request.GetEncodedUrl();
-        if (requestUrl.Contains("/canmanage/") || Regex.IsMatch(requestUrl, @"^/users/\d+$") || Regex.IsMatch(requestUrl, @"^/Users/\d+$") || Regex.IsMatch(requestUrl, @"^places/\d+$/settings") || requestUrl.Contains("/universes/"))
+        if (requestUrl.Contains("/canmanage/") || Regex.IsMatch(requestUrl, @"^/users/\d+$") || Regex.IsMatch(requestUrl, @"^/Users/\d+$") || Regex.IsMatch(requestUrl, @"^/places/\d+$/settings") || requestUrl.Contains("/universes/"))
         {
             await _next(ctx);
             return;
