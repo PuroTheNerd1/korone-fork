4c4fcb43603aef0cdcc89710fec37920107527e6
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-12-02 19:37:44 +0100

    Oops

--- Roblox/Roblox.Website/Middleware/FrontendProxyMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/FrontendProxyMiddleware.cs
@@ -185,7 +185,7 @@ public class FrontendProxyMiddleware
     }
 
     public async Task InvokeAsync(HttpContext ctx)
-    {places/{placeId}
+    {
         var requestUrl = ctx.Request.GetEncodedPathAndQuery();
         var requestFullUrl = ctx.Request.GetEncodedUrl();
         if (requestUrl.Contains("/canmanage/") || Regex.IsMatch(requestUrl, @"^/users/\d+$") || Regex.IsMatch(requestUrl, @"^/Users/\d+$") || Regex.IsMatch(requestUrl, @"^places/\d+$/settings") || requestUrl.Contains("/universes/"))
