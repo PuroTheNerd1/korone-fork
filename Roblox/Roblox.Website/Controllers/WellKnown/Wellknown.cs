c4c6598df47b44ce43307fbfbcb8269679c2bfcf
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-12-17 00:35:40 +0100

    No link for u!

--- Roblox/Roblox.Website/Controllers/WellKnown/Wellknown.cs
+++ Roblox/Roblox.Website/Controllers/WellKnown/Wellknown.cs
@@ -26,25 +26,7 @@ namespace Roblox.Website.Controllers
         [HttpGetBypass(".well-known/discord")]
         public string WellKnownDiscord()
         {
-            string hostName = HttpContext.Request.Host.Host;
-            
-            if (hostName.Contains("phil564"))
-            {
-                return "dh=20873664bf98d2f31ebdd1c2df0ddd62821db03b";
-            }
-            else if (hostName.Contains("zen"))
-            {
-                return "dh=42bc2c49fdf16f30791784a2f16e51c3ed7b5f0c";
-            }
-            else if (hostName.Contains("vmprotect"))
-            {
-                return "dh=696c519db9c551b712e6b516c97bba2c5e3af340";
-            }
-            else if (hostName.Contains("lu"))
-            {
-                return "dh=8eedc367d89168d46cf7d1dadcdc21d26628353a";
-            }
-            return "dh=20873664bf98d2f31ebdd1c2df0ddd62821db03b";
+            return "No link for u";
         }
     }
 }
