d525ce46847612e009ea84beda78fd1d7f534480
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-12-20 14:23:45 +0100

    Remove route

--- Roblox/Roblox.Website/Controllers/RobloxApi/Friends.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Friends.cs
@@ -148,7 +148,6 @@ namespace Roblox.Website.Controllers
         }
 
         [HttpGetBypass("v2/users/{userId}/friends")]
-        [HttpGetBypass("v1/users/{userId}/friends")]
         public async Task<IEnumerable<dynamic>> GetUserFriendsLegacy(long userId)
         {
             var result = await services.friends.GetFriends(userId);
