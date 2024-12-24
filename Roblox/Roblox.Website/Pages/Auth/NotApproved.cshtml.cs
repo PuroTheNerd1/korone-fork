1248142938b1b83387e8928f71753104612ebc4c
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2024-12-24 22:32:53 +0100

    Add master and slaves

--- Roblox/Roblox.Website/Pages/Auth/NotApproved.cshtml.cs
+++ Roblox/Roblox.Website/Pages/Auth/NotApproved.cshtml.cs
@@ -21,7 +21,6 @@ public class NotApproved : RobloxPageModel
         {
             // Ninja low taper fadeeee
             ninjaLowTaperFade = userSession.userId == 3 || userSession.userId == 9|| userSession.userId == 47 || userSession.userId == 1671 || userSession.userId == 2925;
-
             ban = await services.users.GetBanData(userSession.userId);
         }
         catch (RecordNotFoundException)
