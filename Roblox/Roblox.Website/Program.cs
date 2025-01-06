6f93e72ac81b567f2bf8c7390e0087464b2218ac
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-01-06 15:30:02 +0100

    Remove this

--- Roblox/Roblox.Website/Program.cs
+++ Roblox/Roblox.Website/Program.cs
@@ -84,8 +84,7 @@ builder.Services.AddControllers(options =>
 {
     o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     o.JsonSerializerOptions.PropertyNamingPolicy = null;
-})
-.AddXmlSerializerFormatters();
+});
 builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
