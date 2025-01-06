5855c5a9c37391fe924fce307c88ecab334549a8
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-01-06 10:19:52 +0100

    Forgot ,

--- Roblox/Roblox.Website/Middleware/CsrfMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/CsrfMiddleware.cs
@@ -135,7 +135,7 @@ public class CsrfMiddleware : ControllerServicesExtended
         "/moderation/filtertext",
         "/moderation/v2/filtertext",
         "/develop/upload-version",
-        "/AbuseReport/InGameChatHandler.ashx"
+        "/AbuseReport/InGameChatHandler.ashx",
         // universes
         "/universes/",
         // uses built-in RequestVerificationToken
