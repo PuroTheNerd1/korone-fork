d3cc837bd2941a1e79ffeeb8f27e98409b54b783
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-01-06 17:29:54 +0100

    Change c ookie

--- Roblox/Roblox.Website/Controllers/RobloxApi/Login.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Login.cs
@@ -193,7 +193,7 @@ namespace Roblox.Website.Controllers
             HttpContext.Response.Cookies.Append(Middleware.SessionMiddleware.CookieName, sessionCookie, new CookieOptions()
             {
                 Domain = ".pekora.zip",
-                Secure = false,
+                Secure = true,
                 Expires = DateTimeOffset.Now.Add(TimeSpan.FromDays(364)),
                 IsEssential = true,
                 Path = "/",
