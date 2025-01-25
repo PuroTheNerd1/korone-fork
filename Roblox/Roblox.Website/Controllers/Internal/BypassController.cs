e3313c7f2d8277b9d300d07d94524af5d602f8a5
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-01-25 18:22:50 +0100

    remove text

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -641,9 +641,8 @@ namespace Roblox.Website.Controllers
         {
             //do this for anti reporting shit
             if(userSession == null)
-            {
                 return Redirect("/auth/home");
-            }
+
             return Content(await System.IO.File.ReadAllTextAsync("download.html"), "text/html");
         }
 
