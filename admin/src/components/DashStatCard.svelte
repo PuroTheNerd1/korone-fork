63fc0c0d862f6dbdf4f859f0cb0deaa0d126daaa
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-11 15:17:57 +0100

    Update admin panel

--- admin/src/components/DashStatCard.svelte
+++ admin/src/components/DashStatCard.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	export let key: string;
 	export let value: string;
-	export let cardClasses = "bg-dark";
+	export let cardClasses = "mod-card-dark";
 	export let onClick = null;
 </script>
 
 <div class="col-12 col-md-6 col-lg-3 mt-4" on:click={onClick}>
-	<div class={`card ${cardClasses} text-white`}>
+	<div class={`card ${cardClasses}`}>
 		<div class="card-body p-0 pb-3 pl-3">
 			{#if key}
 				<h1 class="m-0 pt-2 pb-2 text-truncate" style="font-size: 4rem;">{key}</h1>
