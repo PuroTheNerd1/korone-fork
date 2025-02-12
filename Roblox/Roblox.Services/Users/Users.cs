37a5d27de0d9d1c9f12a58cc2802b8237792f443
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-12 11:47:44 +0100

    Delete discord account linking

--- Roblox/Roblox.Services/Users/Users.cs
+++ Roblox/Roblox.Services/Users/Users.cs
@@ -227,6 +227,17 @@ public class UsersService : ServiceBase, IService
         {
             // Delete user
             // todo: is creation date technically private info? should we be deleting that too?
+            
+
+            // Delete discord account link
+            string discordId = await db.QuerySingleOrDefaultAsync<string>("SELECT discord_id FROM \"user\" WHERE id = :id", new { id = userId });
+            if (discordId != null)
+            {
+                await db.ExecuteAsync("DELETE FROM discord_link WHERE discord_id = :discord_id", new
+                {
+                    discord_id = discordId,
+                });
+            }
             await db.ExecuteAsync(
                 "UPDATE \"user\" SET username = :name, password = '', status = :status, description = '[ Content Deleted ]', online_at = created_at WHERE id = :id",
                 new
