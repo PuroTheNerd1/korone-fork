ad14c5c9d0a92c8c4350d9d226212fee5c21091b
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-18 02:03:57 +0100

    add promocodeservice and promocodepage

---
+++ Roblox/Roblox.Services/Promocodes.cs
@@ -0,0 +1,74 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Roblox.Dto.Assets;
+using Roblox.Dto.Forums;
+using Roblox.Services.App.FeatureFlags;
+using Roblox.Services.Exceptions;
+
+namespace Roblox.Services;
+
+public class PromocodesService : ServiceBase, IService
+{
+    public async Task AddPromocode(string promocode, long assetId)
+    {
+        await InsertAsync("asset_promocodes", new
+        {
+            asset_id = assetId,
+            promocode,
+        });
+    }
+    public async Task<long> GetAssetIdFromPromocode(string promocode)
+    {
+        return await db.QueryFirstOrDefaultAsync<long>("SELECT asset_id FROM asset_promocodes WHERE promocode = :promocode", new
+        {
+            promocode,
+        });
+    }
+    public async Task<bool> IsPromocodeClaimed(string promocode, long userId)
+    {
+        return await db.QueryFirstOrDefaultAsync<bool>("SELECT 1 FROM user_asset_promocodes WHERE code = :promocode AND user_id = :userId", new
+        {
+            promocode,
+            userId,
+        });
+    }
+    public async Task<long> ClaimPromocode(string promocode, long userId)
+    {
+        long assetId = 0;
+        await InTransaction(async (t) =>
+        {
+            if (await IsPromocodeClaimed(promocode, userId))
+                throw new RecordNotFoundException("Promocode already claimed");
+            // If this failes 
+            await InsertAsync("user_asset_promocodes", new
+            {
+                user_id = userId,
+                code = promocode,
+            });
+            var assetId = await GetAssetIdFromPromocode(promocode);
+            if (assetId == 0)
+                throw new RecordNotFoundException("Invalid promocode");
+            var id = await db.QuerySingleOrDefaultAsync(
+                "INSERT INTO user_asset (asset_id, user_id, serial) VALUES (:asset_id, :user_id, :serial) RETURNING user_asset.id", new
+                {
+                    asset_id = assetId,
+                    user_id = userId,
+                    serial = 0,
+                });
+            assetId = id.id;
+            return 0;
+        });
+        return assetId;
+    }
+    public bool IsReusable()
+    {
+        return true;
+    }
+
+    public bool IsThreadSafe()
+    {
+        return true;
+    }
+}
---
+++ Roblox/Roblox.Website/Pages/Internal/Promocodes.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Roblox.Website.Pages.Internal;
+
+public class Promocodes : RobloxPageModel
+{
+    public string? promocode { get; set; }
+    public string? errorMessage { get; set; }
+    public string? successMessage { get; set; }
+    public void OnGet()
+    {
+
+    }
+    public async Task OnPost()
+    {
+        long assetId = 0;
+        if (string.IsNullOrWhiteSpace(promocode))
+        {
+            errorMessage = "Promocode is empty";
+            return;
+        }
+        try
+        {
+            await services.promocodes.ClaimPromocode(promocode, userSession.userId);
+        }
+        catch (Exception e)
+        {
+            errorMessage = e.Message;
+            return;
+        }
+        var assetInfo = await services.assets.GetAssetCatalogInfo(assetId);
+        successMessage = $"You have successfully claimed the item ({assetInfo.name}! Check your inventory to see it.";
+        
+    }
+}
