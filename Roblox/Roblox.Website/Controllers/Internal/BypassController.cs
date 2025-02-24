13f958898c8809be079463f3b4ae875b13138a5b
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-24 13:07:17 +0100

    Try

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -686,6 +686,21 @@ namespace Roblox.Website.Controllers
         {
             List<long> accessoryVersionIds = new List<long>();
             List<long> equippedGearVersionIds = new List<long>();
+            List<dynamic> emotes = new List<dynamic>
+            {
+                new
+                {
+                    assetId = 41164,
+                    assetName = "Yungblud Happier Jump",
+                    position = 1
+                },
+                new
+                {
+                    assetId = 15610015346,
+                    assetName = "Yungblud Happier Jump#2",
+                    position = 2
+                },
+            };
             var wornAssets = await services.avatar.GetWornAssets(userId);
             var avatar = await services.avatar.GetAvatar(userId);
             // If we dont have an avatar then its most likely a game loading a avatar
@@ -734,12 +749,7 @@ namespace Roblox.Website.Controllers
                 scales,
                 bodyColorsUrl = $"{Configuration.BaseUrl}/Asset/BodyColors.ashx?userId={userId}",
                 bodyColors,
-                emotes = assetInfo.Where(c => c.assetType == Type.EmoteAnimation).Select(c => new
-                {
-                    assetId = c.id,
-                    assetName = c.name,
-                    position = positionCounter++, 
-                }),
+                emotes,
             };
 
             string jsonString = JsonConvert.SerializeObject(result);
