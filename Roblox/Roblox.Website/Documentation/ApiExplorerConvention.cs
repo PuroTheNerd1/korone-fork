f53908bc77ae804c23a95819be3431ecf05c3315
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-25 12:28:59 +0100

    try this

--- Roblox/Roblox.Website/Documentation/ApiExplorerConvention.cs
+++ Roblox/Roblox.Website/Documentation/ApiExplorerConvention.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 public class ApiExplorerGetsOnlyConvention : IActionModelConvention
 {
     public void Apply(ActionModel action)
     {
-        action.ApiExplorer.IsVisible = action.Attributes.OfType<HttpGetAttribute>().Any();
+        var apiExplorerSettings = action.Attributes.OfType<ApiExplorerSettingsAttribute>().FirstOrDefault();
+        if (apiExplorerSettings != null && apiExplorerSettings.IgnoreApi)
+        {
+            action.ApiExplorer.IsVisible = false;
+        }
+        else
+        {
+            action.ApiExplorer.IsVisible = action.Attributes.OfType<HttpGetAttribute>().Any() || action.Attributes.OfType<HttpPostAttribute>().Any();
+        }
     }
 }
