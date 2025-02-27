82aa1240beb9124070ba2899128362675e3a1f14
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-02-27 19:34:26 +0100

    Fix this

--- Roblox/Roblox.Website/Controllers/RobloxApi/FeatureFlagsRoblox.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/FeatureFlagsRoblox.cs
@@ -28,17 +28,6 @@ namespace Roblox.Website.Controllers
             return Content(GetFeatureFlags(applicationName), "application/json");
         }
 
-        // For legacy clients
-        private static readonly Dictionary<string, string> apiKeys = new Dictionary<string, string>()
-        {
-            { "9CE2063F-BB45-449B-89D4-65CD2ED806CD", "RCCServiceUJ38BA31M8F47VA76XZ1RYONSSTILA3F" }, // 2017L RCC
-            { "D6925E56-BFB9-4908-AAA2-A5B1EC4B2D79", "ClientAppSettings2017" },
-            { "D6925E56-BFB9-4908-AAA2-A5B1EC4B2D79", "StudioAppSettings" },
-            { "08BF6621-8100-4484-B14C-87497E372160", "ClientAppSettings2017" }, // 2017L Studio + Client
-            { "D6925E56-BFB9-4908-AAA2-A5B1EC4B2D7A", "RCCService2018" }, // 2018L RCC
-            { "76E5A40C-3AE1-4028-9F10-7C62520BD94F", "ClientAppSettings2018" },
-            { "19C0B314-AC23-4CD4-8A37-02C4140F7240", "ClientAppSettings2018" } // 2018L AppSettings
-        };
         // For modern clients
         private static readonly HashSet<string> applicationNames = new HashSet<string>
         {
@@ -55,9 +44,27 @@ namespace Roblox.Website.Controllers
         };
         private string GetTypeForApiKey(string type, string apiKey)
         {
-            if (!apiKeys.TryGetValue(apiKey, out type))
-                throw new BadRequestException(0, $"Invalid API key: {apiKey}");
-
+            switch (apiKey)
+            {
+                case "9CE2063F-BB45-449B-89D4-65CD2ED806CD":  //2017L RCC
+                    type = "RCCServiceUJ38BA31M8F47VA76XZ1RYONSSTILA3F";
+                    break;
+                case "D6925E56-BFB9-4908-AAA2-A5B1EC4B2D79":
+                case "08BF6621-8100-4484-B14C-87497E372160": //2017L Studio + Client
+                    if(type == "StudioAppSettings")
+                        break;
+                    type = "ClientAppSettings2017";
+                    break;
+                case "D6925E56-BFB9-4908-AAA2-A5B1EC4B2D7A":  //2018L RCC
+                    type = "RCCService2018";
+                    break;
+                case "76E5A40C-3AE1-4028-9F10-7C62520BD94F":
+                case "19C0B314-AC23-4CD4-8A37-02C4140F7240":  ///2018L AppSettings
+                    type = "ClientAppSettings2018";
+                    break;
+                default:
+                    throw new BadRequestException(0, $"Invalid API key: {apiKey}");
+            }
             return type;
         }
         private string GetFeatureFlags(string type, string? apiKey = null)
