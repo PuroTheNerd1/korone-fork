ff2ea1e1b1aa50939f50318e9f7a11f2c5bd949e
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-04-09 13:57:12 +0200

    Hopefully fix

--- Roblox/Roblox.Website/Controllers/RobloxApi/Economy.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Economy.cs
@@ -32,7 +32,7 @@ public class Economy : ControllerBase
     public async Task<dynamic> GetUserCurrency(long userId)
     {
         //FeatureCheck();
-        return await services.economy.GetUserBalance(userSession.userId);
+        return await services.economy.GetUserBalance(safeUserSession.userId);
     }
 
     [HttpGetBypass("v1/assets/{assetId}/users/{userId}/resellable-copies")]
