a3c9f8ed6674fb5f9503d21104d2bd06cc94f9b1
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-04-16 16:28:46 +0200

    Fix gears

--- Roblox/Roblox.Website/Controllers/RobloxApi/Avatar.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Avatar.cs
@@ -86,7 +86,7 @@ public class AvatarRBX : ControllerBase
         equippedGearVersionIds.AddRange(assetInfo.Where(d => d.assetType == Models.Assets.Type.Gear).Select(d => d.id));
         accessoryVersionIds.AddRange(assetInfo.Where(d => d.assetType != Models.Assets.Type.Gear && d.assetType != Models.Assets.Type.EmoteAnimation).Select(d => d.id));
         long placeId = long.Parse(Request.Headers["roblox-place-id"].ToString());
-        if (userId != 0)
+        if (UserAgent != "Roblox/Win2020")
         {
             equippedGearVersionIds = new List<long>();
         }
