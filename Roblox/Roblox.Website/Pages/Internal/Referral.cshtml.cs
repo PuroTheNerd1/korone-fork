e9349f60d6bde787666e947bb3a9c0b74caa8d9a
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-04-21 03:23:56 +0200

    Add sleep

--- Roblox/Roblox.Website/Pages/Internal/Referral.cshtml.cs
+++ Roblox/Roblox.Website/Pages/Internal/Referral.cshtml.cs
@@ -68,6 +68,7 @@ public class Referral : RobloxPageModel
 
                 // Create user refferal with username
                 await services.users.CreateReferralCode(userSession.userId, userSession.username);
+                Thread.Sleep(2000);
                 // Reload sent invites
                 referral = await services.users.GetUserReferral(userSession.userId);
             }
