4f55020848e090a61b8296e7b4155d6bf163ef66
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-05-06 23:54:41 +0200

    Debug

--- Roblox/Roblox.Website/Controllers/v1/Avatar.cs
+++ Roblox/Roblox.Website/Controllers/v1/Avatar.cs
@@ -36,7 +36,7 @@ public class AvatarControllerV1 : ControllerBase, IService
         
         await Task.Run(async () =>
         {
-            await Task.Delay(TimeSpan.FromSeconds(2));
+            //await Task.Delay(TimeSpan.FromSeconds(2));
             Roblox.Models.Avatar.AvatarType? rigType = (Roblox.Models.Avatar.AvatarType?)await services.avatar.GetAvatarTypeAsync(userId);
             using var cache = ServiceProvider.GetOrCreate<AvatarCache>();
             try
