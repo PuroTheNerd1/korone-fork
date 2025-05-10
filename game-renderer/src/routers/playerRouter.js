1ff4da71af34c4cc471426790c200e128e0e713f
johnhamilcar <johnhamilcar@gmail.com>
2025-05-10 09:12:31 -0400

    Update game-renderer to 2.0

--- game-renderer/src/routers/playerRouter.js
+++
@@ -1,9 +0,0 @@
-import xp from 'ultimate-express'
-import { RequestAvatarThumbnail, RequestAvatarHeadshot } from '../controllers/playerRCC.js'
-import authMiddleware from '../middleware/auth.js'
-
-const router = xp.Router();
-router.post(`/thumbnail`, authMiddleware, RequestAvatarThumbnail);
-router.post(`/headshot`, authMiddleware, RequestAvatarHeadshot);
-
-export default router;
--- game-renderer/src/util/response.js
+++
@@ -1,7 +0,0 @@
-export default (res, message, status = 200, success = true, additionalData) => {
-    return res.status(status).send({
-      success,
-      message: message || 'Success',
-      ...additionalData
-    })
-  }
