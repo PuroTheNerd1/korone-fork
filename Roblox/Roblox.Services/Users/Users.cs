abc09d5de402dbc7ccecf5a57bcf2ce373b40c8d
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-05-12 11:57:51 +0200

    username diallow

--- Roblox/Roblox.Services/Users/Users.cs
+++ Roblox/Roblox.Services/Users/Users.cs
@@ -444,6 +444,8 @@ public class UsersService : ServiceBase, IService
         {
             if (normalizedName[i-1] == ' ' && normalizedName[i] == ' ') return false;
         }
+        // check for duplicate underscores
+        if (nameToCheck.Contains("__")) return false;
 
         // world filter, removing spaces and other words
         var lowerName = string.Join("", nameToCheck.ToLower().Split(" "));
