9330dbfb9e7a3612fb4606e53a64f2a61b1da730
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-05-16 17:09:21 +0200

    fix getproductinfo

--- Roblox/Roblox.Website/Controllers/RobloxApi/Marketplace.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Marketplace.cs
@@ -81,37 +81,43 @@ namespace Roblox.Website.Controllers
                 var resale = await services.assets.GetResaleData(assetId);
                 Remaining = resale.numberRemaining;
             }
-
-            // this has gotta be a Type somewhere right
-            return new
+            try
             {
-                TargetId = details.id,
-                AssetId = details.id,
-                ProductId = details.id,
-                Name = details.name,
-                Description = details.description,
-                AssetTypeId = (int)details.assetType,
-                Creator = new
+                // this has gotta be a Type somewhere right
+                return new
                 {
-                    Id = details.creatorTargetId,
-                    Name = details.creatorName,
-                    CreatorType = details.creatorType,
-                    CreatorTargetId = details.creatorTargetId
-                },
-                IconImageAssetId = 0,
-                Created = details.createdAt,
-                Updated = details.updatedAt,
-                PriceInRobux = details.price,
-                PriceInTickets = details.priceTickets,
-                Sales = details.saleCount,
-                IsNew = details.createdAt.Add(TimeSpan.FromDays(1)) < DateTime.Now,
-                IsForSale = details.isForSale,
-                IsPublicDomain = details.isForSale && details.price == 0,
-                IsLimited = details.itemRestrictions.Contains("Limited"),
-                IsLimitedUnique = details.itemRestrictions.Contains("LimitedUnique"),
-                Remaining,
-                MinimumMembershipLevel = 0
-            };
+                    TargetId = details.id,
+                    AssetId = details.id,
+                    ProductId = details.id,
+                    Name = details.name,
+                    Description = details.description,
+                    AssetTypeId = (int)details.assetType,
+                    Creator = new
+                    {
+                        Id = details.creatorTargetId,
+                        Name = details.creatorName,
+                        CreatorType = details.creatorType,
+                        CreatorTargetId = details.creatorTargetId
+                    },
+                    IconImageAssetId = 0,
+                    Created = details.createdAt,
+                    Updated = details.updatedAt,
+                    PriceInRobux = details.price,
+                    PriceInTickets = details.priceTickets,
+                    Sales = details.saleCount,
+                    IsNew = details.createdAt.Add(TimeSpan.FromDays(1)) < DateTime.Now,
+                    IsForSale = details.isForSale,
+                    IsPublicDomain = details.isForSale && details.price == 0,
+                    IsLimited = details.itemRestrictions.Contains("Limited"),
+                    IsLimitedUnique = details.itemRestrictions.Contains("LimitedUnique"),
+                    Remaining,
+                    MinimumMembershipLevel = 0
+                };
+            }
+            catch (RecordNotFoundException)
+            {
+                return Redirect($"https://economy.roproxy.com/v2/assets/{assetId}/details");
+            }
         }
 
         // client here
