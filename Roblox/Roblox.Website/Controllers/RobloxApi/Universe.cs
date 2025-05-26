5fad9f6098055f31098d7c8241ae39c9a2d58c77
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-05-26 12:42:26 +0200

    Oops

--- Roblox/Roblox.Website/Controllers/RobloxApi/Universe.cs
+++ Roblox/Roblox.Website/Controllers/RobloxApi/Universe.cs
@@ -563,7 +563,7 @@ public class UniverseV1 : ControllerBase
     }
     [HttpPostBypass("/v1/universes/{universeId}/teamcreate")]
     [HttpPatchBypass("/v1/universes/{universeId}/teamcreate")]
-    public async Task<dynamic> SetTeamCreateSettings([FromRoute] long universeId, [FromBody] TeamCreateSettings request) 
+    public async Task<dynamic> SetTeamCreateSettings([FromRoute] long universeId) 
     {
         // await services.games.CanManageUniverse(safeUserSession.userId, universeId);
 
