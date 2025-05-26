e26e81882233f87584dcf4fca4ca44500a2d9ea1
unknown <138301021+shikataganaii@users.noreply.github.com>
2025-05-26 11:35:09 +0200

    Bypass group search

--- Roblox/Roblox.Website/Controllers/v1/Groups.cs
+++ Roblox/Roblox.Website/Controllers/v1/Groups.cs
@@ -84,7 +84,7 @@ public class GroupsControllerV1 : ControllerBase
 
     [HttpGet("groups/search")]
     [HttpGet("groups/search/lookup")]
-    [HttpGetBypass("groups/search/lookup")]
+    [HttpGetBypass("/groups/search/lookup")]
     public async Task<dynamic> SearchGroups(string keyword, string? cursor, int limit = 10, bool prioritizeExactMatch = false)
     {
         FeatureCheck();
