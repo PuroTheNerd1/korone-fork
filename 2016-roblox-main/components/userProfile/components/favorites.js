1846d212750caa242ae4eb2bf701f6247d8b128e
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-06-22 18:31:33 +0200

    Gameicons i guess

--- 2016-roblox-main/components/userProfile/components/favorites.js
+++ 2016-roblox-main/components/userProfile/components/favorites.js
@@ -30,15 +30,16 @@ const Favorites = props => {
         setFavorites(d)
       })
 
-      /*multiGetUniverseIcons({
+      multiGetUniverseIcons({
         universeIds: data.Data.Items.map(v => v.Item.UniverseId),
+        size: '150x150'
       }).then(d => {
         let newObj = {}
         for (const item of d) {
           newObj[item.targetId] = item.imageUrl;
         }
         setIcons(newObj);
-      })*/
+      })
     })
   }, [userId]);
 
@@ -54,9 +55,9 @@ const Favorites = props => {
     </div>
     <div className='col-12'>
       {
-        favorites && favorites.length > 0 ? <GameRow games={favorites} />
-          : <div className={`section-content-off`} >User has no favourited games.</div>
-        /*favorites.slice(0,6).map(v => {
+        // favorites && favorites.length > 0 ? <GameRow games={favorites} />
+        //   : <div className={`section-content-off`} >User has no favourited games.</div>
+        favorites.slice(0,6).map(v => {
           return <SmallGameCard
         key={v.Item.AssetId}
         name={v.Item.Name}
@@ -69,7 +70,7 @@ const Favorites = props => {
         hideVoting={true}
         playerCount={0}
       />
-        })*/
+        })
       }
     </div>
   </div>
