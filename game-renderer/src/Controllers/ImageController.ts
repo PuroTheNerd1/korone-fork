c3fdb9d6d56ec1a004b12a97e5a872e2f5d26574
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-07-31 12:16:29 +0200

    Fix

--- game-renderer/src/Controllers/ImageController.ts
+++ game-renderer/src/Controllers/ImageController.ts
@@ -14,7 +14,7 @@ import {Imager} from "../Utilities/Libraries/Imager.js";
 import Resp from "../Utilities/Libraries/Resp.js";
 
 const router = express.Router();
-const box = new QueueBox<express.Response>(`All`, Config.Ports.RCC.All);
+const box = new QueueBox<any>(`All`, Config.Ports.RCC.All);
 
 router.post("/image", Valid(AssetRenderRequest), async (req, res) => {
     const assetUrl = `${Config.BaseUrl}/v1/asset?id=${req.body.assetId}`;
