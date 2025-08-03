8c9b242a6c92712992ae29ea396edfa115e5a253
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-08-03 03:27:58 +0200

    directly put in list

--- Roblox/Roblox.Services/Assets/DataStoreService.cs
+++ Roblox/Roblox.Services/Assets/DataStoreService.cs
@@ -116,13 +116,13 @@ public class DataStoreService : ServiceBase, IService
         var scopeList = keys.Select(k => k.scope).Distinct().ToList();
 
         var result = await db.QueryAsync<DataStoreEntry>(
-            "SELECT id, scope, key, name, value FROM asset_datastore WHERE asset_id = :place_id AND key IN :keys AND scope IN :scope AND name IN :names ORDER BY id DESC LIMIT 250",
-            new
+            "SELECT id, scope, key, name, value FROM asset_datastore WHERE asset_id = @place_id AND key IN @keys AND scope IN @scopes AND name IN @names ORDER BY id DESC LIMIT 250";
+        new
             {
                 place_id = placeId,
-                keys = keyList.ToArray(),
-                scope = scopeList.ToArray(),
-                names = nameList.ToArray(),
+                keys = keyList,
+                scope = scopeList,
+                names = nameList,
             });
         return result;
     }
