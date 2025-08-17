a01c7ed5f3cf4acff69c38ca6fcc2adca7b25d2b
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-08-17 20:04:46 +0200

    Fix getuser

--- Roblox/Roblox.Services/Users/Users.cs
+++ Roblox/Roblox.Services/Users/Users.cs
@@ -602,8 +602,8 @@ public class UsersService : ServiceBase, IService
         var res = await db.QuerySingleOrDefaultAsync<UserInfo>(
             "SELECT id AS userId, username, status AS accountStatus, created_at AS created, description " +
             "FROM \"user\" " +
-            "WHERE LOWER(username) = :name",
-            new { name = username.ToLower() }
+            "WHERE LOWER(username) = LOWER(:username)",
+            new { name = username }
         );
 
         if (res == null)
