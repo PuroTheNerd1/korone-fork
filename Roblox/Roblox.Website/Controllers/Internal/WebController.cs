e7c0af913b0be4c66663ee93f1a42ee844cf1008
shikataganai <138301021+shikataganaii@users.noreply.github.com>
2025-08-23 16:45:27 +0200

    Now it should work

--- Roblox/Roblox.Website/Controllers/Internal/WebController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/WebController.cs
@@ -803,18 +803,23 @@ public class WebController : ControllerBase
 
         try
         {
+            // idk atp
             var fs = request.file.OpenReadStream();
             fs.Position = 0;
             var memoryStream = new MemoryStream();
+            var placeStream = new MemoryStream();
+
             await fs.CopyToAsync(memoryStream);
             fs.Position = 0;
+            await fs.CopyToAsync(placeStream);
+            placeStream.Position = 0;
             memoryStream.Position = 0;
 
             if (!await services.assets.ValidateAssetFile(memoryStream, info.assetType))
                 throw new RobloxException(400, 0, "The asset file doesn't look correct. Please try again.");
 
             memoryStream.Position = 0;
-            await services.assets.CreateAssetVersion(request.assetId, safeUserSession.userId, fs);
+            await services.assets.CreateAssetVersion(request.assetId, safeUserSession.userId, placeStream);
 
             // Render in the background
             //if (info.assetType != Models.Assets.Type.Place) {
