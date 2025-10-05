f081ecdb09a70bc7dab2271e2e830d242b020039
shikataganaii <138301021+shikataganaii@users.noreply.github.com>
2025-10-05 22:23:38 +0200

    Fix gameserver shutdown

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -1349,9 +1349,8 @@ namespace Roblox.Website.Controllers
         {
             if (!isRCC)
                 throw new Roblox.Exceptions.UnauthorizedException(0, "Unauthorized");
-            var gameServer = await services.gameServer.GetGameServer(gameId);
-
-            if ((clientCount == 0 && gameTime > 50) && gameServer != null)
+                
+            if (clientCount == 0 && gameTime > 50)
             {
                 await services.gameServer.ShutDownServerAsync(gameId);
                 return;
