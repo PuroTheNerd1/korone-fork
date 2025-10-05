fee64a65836b9d5988f5b4acd74d2b914902c43c
shikataganaii <138301021+shikataganaii@users.noreply.github.com>
2025-10-05 22:47:28 +0200

    Revert

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -1350,7 +1350,8 @@ namespace Roblox.Website.Controllers
             if (!isRCC)
                 throw new Roblox.Exceptions.UnauthorizedException(0, "Unauthorized");
             var gameServer = await services.gameServer.GetGameServer(gameId);
-            if (clientCount == 0 && gameTime > 50 && gameServer != null)
+
+            if ((clientCount == 0 && gameTime > 50) && gameServer != null)
             {
                 await services.gameServer.ShutDownServerAsync(gameId);
                 return;
