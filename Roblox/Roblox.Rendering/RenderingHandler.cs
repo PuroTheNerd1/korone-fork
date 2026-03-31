b24d797b3986815b3d57bbf4640038f8e50d97e8
shikataganaii <138301021+shikataganaii@users.noreply.github.com>
2026-03-31 13:38:46 +0200

    Migrate renderer to gameserver

--- Roblox/Roblox.Rendering/RenderingHandler.cs
+++ Roblox/Roblox.Rendering/RenderingHandler.cs
@@ -129,7 +129,8 @@ namespace Roblox.Rendering
             }
             // i will add error handling to this later
             var content = new StringContent(JsonSerializer.Serialize(renderRequest), Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PostAsync("http://localhost:3043/" + url, content);
+            // hard coded for now 
+            HttpResponseMessage response = await client.PostAsync("http://45.90.99.26:3043/" + url, content);
             sw.Stop();
             var request = await response.Content.ReadFromJsonAsync<RenderResponse>();
             Console.WriteLine($"[RenderingHandler] Request took {sw.ElapsedMilliseconds}ms");
