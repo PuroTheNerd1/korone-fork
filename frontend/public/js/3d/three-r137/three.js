9170413d286885af4b14dc225a72ca33fa99b266
shikataganaii <138301021+shikataganaii@users.noreply.github.com>
2026-04-13 14:41:25 +0200

    fix 3d finally

--- frontend/public/js/3d/three-r137/three.js
+++ frontend/public/js/3d/three-r137/three.js
@@ -29755,28 +29755,19 @@
 
 		static resolveURL(url, path) {
 			// Invalid URL
-			if (typeof url !== 'string' || url === '') return ''; 
-			
-			// Host Relative URL
+			if (typeof url !== 'string' || url === '') return ''; // Host Relative URL
+
 			if (/^https?:\/\//i.test(path) && /^\//.test(url)) {
 				path = path.replace(/(^https?:\/\/[^\/]+).*/i, '$1');
-			} 
-			
-			// Absolute URL http://,https://,//
-			if (/^(https?:)?\/\//i.test(url)) return url; 
-			
-			// Data URI
-			if (/^data:.*,.*$/i.test(url)) return url; 
-			
-			// Blob URL
-			if (/^blob:.*$/i.test(url)) return url; 
-			
-			// If both path and url contain https://, use url (the second one)
-			if (/^https?:\/\//i.test(path) && /^https?:\/\//i.test(url)) {
-				return url;
-			}
-			
-			// Relative URL
+			} // Absolute URL http://,https://,//
+
+
+			if (/^(https?:)?\/\//i.test(url)) return url; // Data URI
+
+			if (/^data:.*,.*$/i.test(url)) return url; // Blob URL
+
+			if (/^blob:.*$/i.test(url)) return url; // Relative URL
+
 			return path + url;
 		}
 
