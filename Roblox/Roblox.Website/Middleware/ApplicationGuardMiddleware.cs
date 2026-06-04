0b46f27193a5874287340eeee5abbb14a05e4cc6
ppc32el <183505371+ppc32el@users.noreply.github.com>
2026-06-04 09:46:01 +0300

    [fix] whitelist setting/quietget

--- Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/ApplicationGuardMiddleware.cs
@@ -303,7 +303,7 @@ public class ApplicationGuardMiddleware
         var uaBlocked = IsUserAgentBlocked(ua);
         if(uaBlocked)
         {
-            uaBlocked = !ctx.Request.GetEncodedUrl().ToString().Contains("clientsettings");
+            uaBlocked = !ctx.Request.GetEncodedPathAndQuery().Contains("setting/quietget/");
         };
 
         var bypassOk = false;
