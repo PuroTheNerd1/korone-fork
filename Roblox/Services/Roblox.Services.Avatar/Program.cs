3bb29a91e23681d63041570cc5e2041b31ee3b29
ppc32el <183505371+ppc32el@users.noreply.github.com>
2026-06-08 09:13:10 +0300

    [fix] feature flags refresh (needs refactor)

--- Roblox/Services/Roblox.Services.Avatar/Program.cs
+++ Roblox/Services/Roblox.Services.Avatar/Program.cs
@@ -1,10 +1,15 @@
 using Roblox.ServiceDefaults;
+using Roblox.Services.App.FeatureFlags;
 using Roblox.Services.Avatar.ExceptionHandlers;
+using Roblox.Services.Avatar.HostedServices;
 using Roblox.Web.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddRobloxServiceDefaults("Roblox.Services.Avatar", ServiceExposure.InternalService);
+await FeatureFlags.RefreshOnceAsync();
+
+builder.Services.AddHostedService<FeatureFlagRefreshHostedService>();
 builder.Services.AddExceptionHandler<AvatarServiceExceptionHandler>();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
