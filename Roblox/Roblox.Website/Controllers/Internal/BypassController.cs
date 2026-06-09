f1ec000b9436db4715bab2289a281bca0d126cb3
ppc32el <183505371+ppc32el@users.noreply.github.com>
2026-06-09 12:35:21 +0300

    i hate everything

--- Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
+++ Roblox/Roblox.Website/Controllers/Internal/BypassController.cs
@@ -697,7 +697,7 @@ namespace Roblox.Website.Controllers
                 throw new ForbiddenException(0, "User is banned");
             }
 
-            await services.gameServer.OnPlayerJoin(visitorId, placeId, gameId, userInfo.username);
+            await services.gameServer.OnPlayerJoin(visitorId, placeId, gameId);
             return Ok();
         }
 
@@ -707,7 +707,7 @@ namespace Roblox.Website.Controllers
             if (!isRCC)
                 throw new UnauthorizedAccessException();
             var jobId = await services.gameServer.GetJobIdByUserId(visitorId);
-            long placeId = await services.gameServer.GetUserPlaceIdAsync(visitorId);
+            long placeId = GameServerService.GetUserPlaceId(visitorId);
 
             await services.gameServer.OnPlayerLeave(visitorId, placeId, jobId);
         }
