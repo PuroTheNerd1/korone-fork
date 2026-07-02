16bbe65a7992cd3070fa395af7d2b498aa83c4e6
ppc32el <183505371+ppc32el@users.noreply.github.com>
2026-07-02 12:52:51 +0300

    add ratelimit to games recommendations api

--- Roblox/Roblox.Website/Controllers/v1/Games.cs
+++ Roblox/Roblox.Website/Controllers/v1/Games.cs
@@ -216,6 +216,9 @@ public class GamesControllerV1 : ControllerBase
     [HttpGet("games/recommendations/game/{universeId:long}")]
     public async Task<dynamic> GetRecommendedGames(long universeId, int maxRows = 6)
     {
+        if (!await services.cooldown.TryIncrementBucketCooldown("Games:Recommendations:Game:Ip:" + GetIP(), 1000, TimeSpan.FromMinutes(1)))
+            throw new RobloxException(RobloxException.TooManyRequests);
+        
         if (maxRows is > 50 or < 1) maxRows = 50;
         // todo: actually add recommendeds
         var result = await services.games.GetGamesList(safeUserSession.userId, "popular", maxRows, null, null);
